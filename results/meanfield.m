98cc45e5950cd34b8fb67ff9cab11f7520edefbf
Harvey Coplestone <harveycoplestone@me.com>
2019-05-06 02:46:56 +0100

    report

--- results/meanfield.m
+++ results/meanfield.m
@@ -10,9 +10,37 @@ EnergyPerSite = data(:,3);
 SpecificHeat = data(:,4);
 MagneticSusceptibility = data(:,5);
 
-
-plot(Temperature, Magnetisation, 'x-')
-
-hold off;
-xlabel('$\frac{\beta}{\beta_c}$', 'Interpreter', 'latex', 'FontSize', 16);
+figure;
+plot(Temperature, Magnetisation, '.-')
+xlabel('$\frac{T}{T_c}$', 'Interpreter', 'latex', 'FontSize', 16);
 ylabel('$\mathcal{M}$', 'Interpreter', 'latex', 'FontSize', 16);
+ax = gca;
+ax.FontSize = 14; 
+
+figure;
+plot(Temperature, EnergyPerSite, '.-')
+xlabel('$\frac{T}{T_c}$', 'Interpreter', 'latex', 'FontSize', 16);
+ylabel('$\frac{E}{NJ}$', 'Interpreter', 'latex', 'FontSize', 16);
+ax = gca;
+ax.FontSize = 14; 
+
+figure;
+plot(Temperature, SpecificHeat, '.-')
+xlabel('$\frac{T}{T_c}$', 'Interpreter', 'latex', 'FontSize', 16);
+ylabel('$\frac{c}{k_B}$', 'Interpreter', 'latex', 'FontSize', 16);
+ax = gca;
+ax.FontSize = 14; 
+
+figure;
+plot(Temperature, MagneticSusceptibility, '.-')
+xlabel('$\frac{T}{T_c}$', 'Interpreter', 'latex', 'FontSize', 16);
+ylabel('$\chi$', 'Interpreter', 'latex', 'FontSize', 16);
+ax = gca;
+ax.FontSize = 14; 
+
+% hold off;
+% xlabel('$\frac{T}{T_c}$', 'Interpreter', 'latex', 'FontSize', 16);
+% % ylabel('$\mathcal{M}$', 'Interpreter', 'latex', 'FontSize', 16);
+% % ylabel('$\frac{E}{NJ}$', 'Interpreter', 'latex', 'FontSize', 16);
+% % ylabel('$\frac{c}{k_B}$', 'Interpreter', 'latex', 'FontSize', 16);
+% ylabel('$\chi$', 'Interpreter', 'latex', 'FontSize', 16);
